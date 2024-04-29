fae1dd311b52be19adfb5bf2563147ba3a9c1445
Dini Putri <mahardini.rizky@gmail.com>
2024-04-30 01:20:43 +1000

    Trash functionality with archiving, restoring, and removing documents

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,9 +1,24 @@
 'use client'
 
+import { useUser } from '@clerk/clerk-react'
+import { DropdownMenuSeparator } from '@radix-ui/react-dropdown-menu'
 import { useMutation } from 'convex/react'
-import { ChevronDown, ChevronRight, LucideIcon, Plus } from 'lucide-react'
+import {
+  ChevronDown,
+  ChevronRight,
+  LucideIcon,
+  MoreHorizontal,
+  Plus,
+  Trash,
+} from 'lucide-react'
 import { toast } from 'sonner'
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
 import { Skeleton } from '@/components/ui/skeleton'
 import { api } from '@/convex/_generated/api'
 import { Id } from '@/convex/_generated/dataModel'
@@ -12,7 +27,7 @@ import { cn } from '@/lib/utils'
 interface ItemProps {
   id?: Id<'documents'>
   label: string
-  onClick: () => void
+  onClick?: () => void
   icon: LucideIcon
   documentIcon?: string // emoji
   active?: boolean
@@ -34,12 +49,15 @@ const Item = ({
   level = 0,
   onExpand,
 }: ItemProps) => {
+  const { user } = useUser()
   // const router = useRouter()
   const create = useMutation(api.documents.create)
+  const archive = useMutation(api.documents.archive)
+
   const ChevronIcon = expanded ? ChevronDown : ChevronRight
 
-  const handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    e.stopPropagation()
+  const handleClick = () => {
+    // e.stopPropagation()
     onClick?.()
   }
 
@@ -66,6 +84,20 @@ const Item = ({
       loading: 'Creating a new note...',
       success: 'New note created!',
       error: 'Failed to create a new note.',
+    })
+  }
+
+  const onArchive = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    e.stopPropagation()
+
+    if (!id) return
+
+    const promise = archive({ id })
+
+    toast.promise(promise, {
+      loading: 'Moving to Trash...',
+      success: 'Note moved to Trash!',
+      error: 'Failed to archive note.',
     })
   }
 
@@ -101,6 +133,31 @@ const Item = ({
       )}
       {!!id && (
         <div className='ml-auto flex items-center gap-x-2'>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild onClick={(e) => e.stopPropagation()}>
+              <div
+                role='button'
+                className='opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:bg-neutral-600'
+              >
+                <MoreHorizontal className='h-4 w-4 text-muted-foreground' />
+              </div>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+              className='w-60'
+              align='start'
+              side='right'
+              forceMount
+            >
+              <DropdownMenuItem onClick={onArchive}>
+                <Trash className='h-4 w-4 mr-2' />
+                Delete
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <div className='text-sm text-muted-foreground p-2'>
+                Last edited by: {user?.fullName}
+              </div>
+            </DropdownMenuContent>
+          </DropdownMenu>
           <div
             role='button'
             onClick={onCreate}
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -4,20 +4,28 @@ import { useMutation } from 'convex/react'
 import {
   ChevronsLeft,
   MenuIcon,
+  Plus,
   PlusCircle,
   Search,
   Settings,
+  Trash,
 } from 'lucide-react'
 import { usePathname } from 'next/navigation'
 import { ElementRef, useEffect, useRef, useState } from 'react'
 import { toast } from 'sonner'
 import { useMediaQuery } from 'usehooks-ts'
 
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
 import { api } from '@/convex/_generated/api'
 import { cn } from '@/lib/utils'
 
 import DocumentList from './document-list'
 import Item from './item'
+import TrashBox from './trash-box'
 import UserItem from './user-item'
 
 const Navigation = () => {
@@ -146,6 +154,18 @@ const Navigation = () => {
         </div>
         <div className='mt-4'>
           <DocumentList />
+          <Item onClick={handleCreate} icon={Plus} label='Add a page' />
+          <Popover>
+            <PopoverTrigger className='w-full mt-4'>
+              <Item label='Trash' icon={Trash} />
+            </PopoverTrigger>
+            <PopoverContent
+              className='p-0 w-72'
+              side={isMobile ? 'bottom' : 'right'}
+            >
+              <TrashBox />
+            </PopoverContent>
+          </Popover>
         </div>
         <div />
         <div
